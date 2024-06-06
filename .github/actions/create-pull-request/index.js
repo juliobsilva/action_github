72fa9ff7912fbd29c06d1d7a3e6537655e6d0d6d
Julio César Barbosa da Silva <j.barbosa.da.silva@avanade.com>
2024-06-06 15:22:41 -0300

    Actions

--- .github/actions/create-pull-request/index.js
+++ .github/actions/create-pull-request/index.js
@@ -1,29 +1,30 @@
-// .github/actions/create-pull-request/index.js
 const core = require('@actions/core');
 const exec = require('@actions/exec');
+const fs = require('fs');
 
-try {
-  const githubToken = core.getInput('github-token');
-  const branchName = core.getInput('branch-name');
-  const prTitle = core.getInput('pr-title');
-  const prBody = core.getInput('pr-body');
+async function run() {
+  try {
+    const githubToken = core.getInput('github-token');
+    const branchName = core.getInput('branch-name');
+    const prTitle = core.getInput('pr-title');
+    const prBody = core.getInput('pr-body');
 
-  // Login no GitHub CLI
-  // Wrap the code in an async function
-  async function run() {
-    await exec.exec('echo', [githubToken, '|', 'gh', 'auth', 'login', '--with-token']);
+    // Login no GitHub CLI
+    await exec.exec('sh', ['-c', `echo ${githubToken} | gh auth login --with-token`]);
+
+    // Criar e mudar para a nova branch
     await exec.exec('git', ['checkout', '-b', branchName]);
 
     // Adicionar um arquivo com run_id
     const runId = process.env.GITHUB_RUN_ID;
     const filename = `run-id-${runId}.txt`;
-    await exec.exec('echo', [`run-id-${runId}`, '>>', filename]);
+    fs.writeFileSync(filename, `run-id-${runId}\n`);
 
     // Adicionar e commitar o arquivo
     await exec.exec('git', ['add', filename]);
     await exec.exec('git', ['config', '--global', 'user.email', 'APIOPS-Extractor@noreply.com']);
     await exec.exec('git', ['config', '--global', 'user.name', 'APIOPS Extractor']);
-    await exec.exec('git', ['commit', '-m', 'Add ' + filename]);
+    await exec.exec('git', ['commit', '-m', `Add ${filename}`]);
 
     // Push da nova branch
     await exec.exec('git', ['push', '--set-upstream', 'origin', branchName]);
@@ -32,13 +33,9 @@ try {
     await exec.exec('gh', ['pr', 'create', '--title', prTitle, '--body', prBody, '--head', branchName]);
 
     core.setOutput('pr-url', `https://github.com/${process.env.GITHUB_REPOSITORY}/pull/${runId}`);
-  }
-
-  // Call the function
-  run().catch(error => {
+  } catch (error) {
     core.setFailed(error.message);
-  });
-
-} catch (error) {
-  core.setFailed(error.message);
+  }
 }
+
+run();
