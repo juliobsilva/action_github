4c21dd354b3753de374ffc72af98b1c18e7748aa
Julio César Barbosa da Silva <j.barbosa.da.silva@avanade.com>
2024-06-06 15:18:08 -0300

    Actions

--- .github/actions/create-pull-request/index.js
+++ .github/actions/create-pull-request/index.js
@@ -13,30 +13,32 @@ try {
   async function run() {
     await exec.exec('echo', [githubToken, '|', 'gh', 'auth', 'login', '--with-token']);
     await exec.exec('git', ['checkout', '-b', branchName]);
+
+    // Adicionar um arquivo com run_id
+    const runId = process.env.GITHUB_RUN_ID;
+    const filename = `run-id-${runId}.txt`;
+    await exec.exec('echo', [`run-id-${runId}`, '>>', filename]);
+
+    // Adicionar e commitar o arquivo
+    await exec.exec('git', ['add', filename]);
+    await exec.exec('git', ['config', '--global', 'user.email', 'APIOPS-Extractor@noreply.com']);
+    await exec.exec('git', ['config', '--global', 'user.name', 'APIOPS Extractor']);
+    await exec.exec('git', ['commit', '-m', 'Add ' + filename]);
+
+    // Push da nova branch
+    await exec.exec('git', ['push', '--set-upstream', 'origin', branchName]);
+
+    // Criar o Pull Request
+    await exec.exec('gh', ['pr', 'create', '--title', prTitle, '--body', prBody, '--head', branchName]);
+
+    core.setOutput('pr-url', `https://github.com/${process.env.GITHUB_REPOSITORY}/pull/${runId}`);
   }
 
   // Call the function
-  run().catch(console.error);
+  run().catch(error => {
+    core.setFailed(error.message);
+  });
 
-
-  // Adicionar um arquivo com run_id
-  const runId = process.env.GITHUB_RUN_ID;
-  const filename = `run-id-${runId}.txt`;
-  await exec.exec('echo', [`run-id-${runId}`, '>>', filename]);
-
-  // Adicionar e commitar o arquivo
-  await exec.exec('git', ['add', filename]);
-  await exec.exec('git', ['config', '--global', 'user.email', 'APIOPS-Extractor@noreply.com']);
-  await exec.exec('git', ['config', '--global', 'user.name', 'APIOPS Extractor']);
-  await exec.exec('git', ['commit', '-m', 'Add ' + filename]);
-
-  // Push da nova branch
-  await exec.exec('git', ['push', '--set-upstream', 'origin', branchName]);
-
-  // Criar o Pull Request
-  await exec.exec('gh', ['pr', 'create', '--title', prTitle, '--body', prBody, '--head', branchName]);
-
-  core.setOutput('pr-url', `https://github.com/${process.env.GITHUB_REPOSITORY}/pull/${runId}`);
 } catch (error) {
   core.setFailed(error.message);
 }
